6760118e3071f0c633ecd62657d6727a01ed361b
Sam Roberts <samroberts@owners-MacBook-Pro.local>
2019-02-19 18:19:56 +0800

    Added the js carousel

--- carousel.js
+++ carousel.js
@@ -1,23 +1,22 @@
-//Sliders//
 $(document).ready(function(){
-  $('.customer-logos').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      arrows: false,
-      dots: false,
-      pauseOnHover: false,
-      responsive: [{
-          breakpoint: 768,
-          settings: {
-              slidesToShow: 1
-          }
-      }, {
-          breakpoint: 520,
-          settings: {
-              slidesToShow: 1
-          }
-      }]
-  });
+    $('.customer-logos').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 1
+            }
+        }]
+    });
 });

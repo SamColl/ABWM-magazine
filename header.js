bfb88ddb9b2027538856e508a8d39aed1fad6efa
Sam Roberts <samroberts@owners-MacBook-Pro.local>
2019-02-23 19:44:57 +0800

    changed header images

--- header.js
+++ header.js
@@ -1,11 +1,11 @@
 $(document).on("scroll", function(){
 		if
       ($(document).scrollTop() > 100){
-      $("header img").removeClass("header")
-		  $("header").addClass("shrink");
+      $("header img").removeClass("header", 1000)
+		  $("header").addClass("shrink", 1000);
 		}
 		else
 		{
-			$("header").removeClass("shrink");
+			$("header").removeClass("shrink", 1000);
 		}
 	});

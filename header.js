091359f22254f6c34e50b440cd3dd1f26cfcd838
Sam Roberts <samroberts@owners-MacBook-Pro.local>
2019-02-25 14:37:28 +0800

    added another headertest file.

--- header.js
+++ header.js
@@ -1,15 +1,14 @@
 
 
 
-
-$(document).on("scroll", function(){
-		if
-      ($(document).scrollTop() > 100){
-      $("header img").removeClass("header", 1000)
-		  $("header").addClass("shrink", 1000);
-		}
-		else
-		{
-			$("header").removeClass("shrink", 1000);
-		}
-	});
+		$(document).on("scroll", function(){
+				if
+		      ($(document).scrollTop() > 100){
+		      $("header img").removeClass("header", 1000)
+				  $("header").addClass("shrink", 1000);
+				}
+				else
+				{
+					$("header").removeClass("shrink", 1000);
+				}
+			});
